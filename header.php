d6b3a72f53ed3a1d479dd90f8e9d7e4b6e06f262
Rob van der Heijden <413117@student.fontys.nl>
2019-02-25 13:46:32 +0100

    update header

---
+++ header.php
@@ -0,0 +1,10 @@
+<?php echo "asdasd" ?>
+<div class="header">
+	<div class="header-logo-div">
+		<a href="./index.html "><img src="logo-dierenasiel.png" alt="logo-dierenasiel"></a>
+	</div>
+	<div class="header-slogan-div">
+		<h1 class="header-title">Dierenasiel Bunky!!</h1>
+		<p class="header-slogan">"Dieren zijn voor iedereen"</p>
+	</div>
+</div>
